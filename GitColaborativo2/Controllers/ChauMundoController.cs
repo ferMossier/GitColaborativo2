7c29378fd4cec28bd7aa944bbe09f3f0b15e2200
FerMossier <mossierfernando@gmail.com>
2024-01-08 15:51:42 -0300

    Se crea funcionalidad de Chau Mundo

---
+++ GitColaborativo2/Controllers/ChauMundoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitColaborativo2.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class ChauMundoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetChauMundo()
+        {
+            return Ok( new { Data = "Chau Mundo" } );
+        }
+    }
+}
